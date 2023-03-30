aba024dbab6428edbfeb84e556bec3630f434dbd
Ales Povalac <alpov@alpov.net>
2023-03-30 11:41:46 +0200

    Updated parsing scripts

--- run-convert.sh
+++ run-convert.sh
@@ -13,8 +13,10 @@ awk 'NR>=150873 && NR <= 529796 { print }' $WORKDIR/log.txt > $WORKDIR/log/01_Br
 awk 'NR>=636205 && NR <= 886868 { print }' $WORKDIR/log.txt > $WORKDIR/log/02_Liege.log
 awk 'NR>=947787 && NR <= 979382 { print }' $WORKDIR/log.txt > $WORKDIR/log/03_Brno_join.log
 awk 'NR>=1015139 && NR <= 1178855 { print }' $WORKDIR/log.txt > $WORKDIR/log/04_Graz.log
-awk 'NR>=1179308 { print }' $WORKDIR/log.txt > $WORKDIR/log/05_Wien.log
-#3657105
+awk 'NR>=1179308 && NR <= 3657105 { print }' $WORKDIR/log.txt > $WORKDIR/log/05_Wien.log
+#awk 'NR>=3657108 && NR <= 4969648 { print }' $WORKDIR/log.txt > $WORKDIR/log/06_Brno_dirty.log
+awk 'NR>=4969648 && NR <= 6919392 { print }' $WORKDIR/log.txt > $WORKDIR/log/07_Brno.log
+
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/
 
